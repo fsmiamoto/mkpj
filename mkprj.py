1024e9519716cbbfa091b750cff137e7fb4e8d8b
Francisco Miamoto <fsmiamoto@gmail.com>
2019-08-17 23:50:50 -0300

    Changes to error handling

--- mkprj.py
+++ mkprj.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 
 from os import getenv, mkdir
-from os.path import join, dirname, exists
+from os.path import join, exists
 import argparse
 
 from dotenv import load_dotenv
 from github import Github
 import git
 
+# Access $HOME environment var.
+HOME_DIR = getenv('HOME')
+
 # Load dotenv file
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(HOME_DIR, '.env')
 load_dotenv(dotenv_path)
 
 # GitHub personal access key
 GITHUB_KEY = getenv('GITHUB_KEY')
-
-# Access $HOME environment var.
-HOME_DIR = getenv('HOME')
 
 # Projects directory
 PROJECTS_DIR = join(HOME_DIR, "Dev")
@@ -46,9 +46,19 @@ def create_new_github_repo(name: str, description: str, private: bool) -> str:
     g = Github(GITHUB_KEY)
     user = g.get_user()
 
-    new_repo = user.create_repo(name, description=description, private=private)
+    try:
+        user.get_repo(name)
+        print('mkdir: GitHub repo already exists!')
+        return
+    except Exception:
+        if description is not None:
+            new_repo = user.create_repo(name,
+                                        description=description,
+                                        private=private)
+        else:
+            new_repo = user.create_repo(name, private=private)
 
-    return new_repo.ssh_url
+        return new_repo.ssh_url
 
 
 if __name__ == '__main__':
@@ -67,7 +77,9 @@ if __name__ == '__main__':
         if args.github:
             remote_url = create_new_github_repo(args.name, args.description,
                                                 args.private)
-            r.create_remote('origin', remote_url)
+            if remote_url is not None:
+                r.create_remote('origin', remote_url)
+
     else:
         print('mkprj: Project with this name already exists!')
 
